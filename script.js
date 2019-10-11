6549822b8f204399becb0a653fc1e30d1019c0c5
EyeTyrant <1414jg@gmail.com>
2019-10-11 12:46:49 -0500

    Class 16 Studio ready to submit

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 window.addEventListener('load', function(){
   fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
     response.json().then(function(json){
-      const astro = document.getElementById('container');
-      // console.log(json)
+      // Bonus Mission 1
       let sorted = json.sort((a,b) => (a.hoursInSpace < b.hoursInSpace) ? 1 : -1);
-      
+      // Bonus Mission 3
       let count = document.getElementById('astCount');
       count.innerHTML = `<h2>Count: ${json.length}</h2>`;
+
+      const astro = document.getElementById('container');
       for (let i = 0; i < json.length; i++){
+        // Bonus Mission 2
         let isActiveColor = json[i].active ? 'green' : '';
+        
         astro.innerHTML += `
         <div class='astronaut'>
           <div class='bio'>
