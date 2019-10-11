a1cda6042505b8be34f8a85af56e2282214ac58a
J G <1414jg@gmail.com>
2019-10-10 20:08:31 -0500

    Class 16 Studio ready for submit

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ window.addEventListener('load', function(){
   fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response){
     response.json().then(function(json){
       const astro = document.getElementById('container');
-      console.log(json)
+      // console.log(json)
+      let sorted = json.sort((a,b) => (a.hoursInSpace < b.hoursInSpace) ? 1 : -1);
+      
+      let count = document.getElementById('astCount');
+      count.innerHTML = `<h2>Count: ${json.length}</h2>`;
       for (let i = 0; i < json.length; i++){
-        console.log(json[i])
+        let isActiveColor = json[i].active ? 'green' : '';
         astro.innerHTML += `
         <div class='astronaut'>
           <div class='bio'>
             <h3>${json[i].firstName} ${json[i].lastName}</h3>
             <ul>
               <li>Hours in space: ${json[i].hoursInSpace}</li>
-              <li>Active: ${json[i].active}</li>
+              <li id='isActive' style="color: ${isActiveColor};">Active: ${json[i].active}</li>
               <li>Skills: ${(json[i].skills.join(', '))}</li>
             </ul>
           </div>
-          <img class='avatar' src='images/${json[i].picture}'</img>
+          <img class='avatar' src='${json[i].picture}'</img>
         </div>
           `;
       };
